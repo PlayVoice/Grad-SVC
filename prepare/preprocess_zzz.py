4705a4ea83b4dcd05c9bee29536c944cdd7940a8
MaxFish <16432329+MaxMax2016@users.noreply.github.com>
2023-09-26 09:23:12 +0800

    Update preprocess_zzz.py

--- prepare/preprocess_zzz.py
+++ prepare/preprocess_zzz.py
@@ -5,26 +5,27 @@ from torch.utils.data import DataLoader
 from grad_extend.data import TextMelSpeakerDataset, TextMelSpeakerBatchCollate
 
 
-filelist_path = "files/valid.txt"
-
-dataset = TextMelSpeakerDataset(filelist_path)
-collate = TextMelSpeakerBatchCollate()
-loader = DataLoader(dataset=dataset, 
-                    batch_size=2,
-                    collate_fn=collate, 
-                    drop_last=True,
-                    num_workers=1, 
-                    shuffle=True)
-
-for batch in tqdm(loader):
-    lengths = batch['lengths'].cuda()
-    vec = batch['vec'].cuda()
-    pit = batch['pit'].cuda()
-    spk = batch['spk'].cuda()
-    mel = batch['mel'].cuda()
-
-    print('len', lengths.shape)
-    print('vec', vec.shape)
-    print('pit', pit.shape)
-    print('spk', spk.shape)
-    print('mel', mel.shape)
+if __name__ == "__main__":
+    filelist_path = "files/valid.txt"
+    
+    dataset = TextMelSpeakerDataset(filelist_path)
+    collate = TextMelSpeakerBatchCollate()
+    loader = DataLoader(dataset=dataset, 
+                        batch_size=2,
+                        collate_fn=collate, 
+                        drop_last=True,
+                        num_workers=1, 
+                        shuffle=True)
+    
+    for batch in tqdm(loader):
+        lengths = batch['lengths'].cuda()
+        vec = batch['vec'].cuda()
+        pit = batch['pit'].cuda()
+        spk = batch['spk'].cuda()
+        mel = batch['mel'].cuda()
+    
+        print('len', lengths.shape)
+        print('vec', vec.shape)
+        print('pit', pit.shape)
+        print('spk', spk.shape)
+        print('mel', mel.shape)
